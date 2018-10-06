526072b22c3e9373cfc3bf3ee23ac29843c03f83
venu2804 <venu2804@gmail.com>
2018-10-06 19:18:40 +0100

    First Commit

---
+++ src/academy/learnProgramming/ConflictExample.java
@@ -0,0 +1,22 @@
+package academy.learnProgramming;
+
+import java.util.Date;
+import java.util.List;
+
+
+public class ConflictExample {
+
+    public static void main(String[] args) {
+
+        Date mydate;
+
+        java.sql.Date anotherDate;
+    }
+
+    public void checkEmpty(List list) {
+        if (list.isEmpty()){
+            System.out.println("Empty");
+        } else { System.out.println("Not Empty");
+    }
+
+}
